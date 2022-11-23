440494c8868d5dc88ba893de81779f7dff7daf65
Behieril <Behieril@users.noreply.github.com>
2022-11-23 14:42:01 +0000

    deploy: e6bc74f8d4a595013c8d9f777731e1810a7361f7

--- test/specs/example.e2e.js
+++
@@ -1,9 +0,0 @@
-const LoginPage = require('../pageobjects/login.page');
-const axios = require('axios');
-
-describe('api request from wdio ', () => { 
-    it('check Email', async () => { 
-    const message = await LoginPage.checkMesages("fghxxcbv@mailsac.com") });});
-    
-
-
