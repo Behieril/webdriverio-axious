f370738a4052199cf2d4a26586715b417971dcdc
Bobkov Roman <roman.bobokov@gmail.com>
2022-11-23 16:32:03 +0200

    Fix 2

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,52 +1,92 @@
+const allure = require('allure-commandline');
+
 exports.config = {
     runner: 'local',
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.js',
+        //'./test/specs/structure/StructureProperties.spec.js'
     ],
 
+    // Patterns to exclude.
     exclude: [
-
+        // 'path/to/excluded/files'
     ],
-
-    maxInstances: 10,
-
+    maxInstances: 5,
     capabilities: [{
-
-        maxInstances: 5,
-        //
+        maxInstances: 1,
         browserName: 'chrome',
         acceptInsecureCerts: true
-
-    }],
-
-    logLevel: 'info',
-
+    },
+    ],
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    logLevel: 'warn',
     bail: 0,
-
     baseUrl: 'http://localhost',
-    //
-    // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
-
+    waitforTimeout: 20000,
     connectionRetryTimeout: 120000,
-
     connectionRetryCount: 3,
-
-    services: ['chromedriver'],
-
+    services: [
+        ['chromedriver']
+    ],
     framework: 'mocha',
+    // reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+    }], 'spec'],
 
-    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
-
+    //
+    // Options to be passed to Mocha.
+    // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 60000 // 20 min
     },
+    // =====
+    // Hooks
+    // =====
 
+    beforeSession: function (config, capabilities, specs) {
+    },
+    beforeSession() { // before hook works as well
+        require('expect-webdriverio').setOptions({
+            wait: 5000
+        })
+    },
+    before: async function (capabilities, specs) {
+        await browser.setTimeout({ 'pageLoad': 20000 })
+    },
     afterTest: async function(test, context, { error, result, duration, passed, retries }) {
-        if (!passed) {
+        if (error) {
             await browser.takeScreenshot();
         }
     },
+    // afterStep: function (test, scenario, { error, duration, passed }) {
+    //     browser.takeScreenshot();
+    // },
 
-}
+    // onComplete: function(exitCode, config, capabilities, results) {
+    // },
+    // onComplete: async function() {
+    //     const reportError = new Error('Could not generate Allure report')
+    //     const generation = allure(['generate', 'allure-results', '--clean'])
+
+    //     return await new Promise(async (resolve, reject) => {
+    //         const generationTimeout = setTimeout(
+    //             () => reject(reportError),
+    //             5000)
+    //         exec('cp -R allure-report/history allure-results');
+    //         generation.on('exit', function(exitCode) {
+    //             clearTimeout(generationTimeout)
+
+    //             if (exitCode !== 0) {
+    //                 return reject(reportError)
+    //             }
+
+    //             console.log('Allure report successfully generated')
+    //             resolve()
+    //         });
+    //     })
+    // }
+};
