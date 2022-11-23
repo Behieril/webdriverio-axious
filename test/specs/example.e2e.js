4649a299f27fc5ff128e280ec5a7cc41b9166bbf
Bobkov Roman <roman.bobokov@gmail.com>
2022-11-23 16:15:08 +0200

    Main files

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,9 @@
+const LoginPage = require('../pageobjects/login.page');
+const axios = require('axios');
+
+describe('api request from wdio ', () => { 
+    it('check Email', async () => { 
+    const message = await LoginPage.checkMesages("fghxxcbv@mailsac.com") });});
+    
+
+
